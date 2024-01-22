1bdbb0ca1ff2c8886f2dbc5f25f22a41865a9213
Samara1404 <stambekova1404@gmail.com>
2024-01-22 23:57:57 +0300

    hair stylist

---
+++ lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hair_stylist/style.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required String title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Hair stylist', style: AppTextStyle.styleApp1,),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppTextStyle{
+  static const TextStyle styleApp = TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white);
+  static const TextStyle styleApp1 = TextStyle(fontSize: 22, fontWeight: FontWeight.w500, color: Colors.black);
+}
